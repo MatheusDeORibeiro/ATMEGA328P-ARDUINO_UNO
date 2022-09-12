97daf17672da326e73529a47bd9769c046dc74e3
MatheusDeORibeiro <89406578+MatheusDeORibeiro@users.noreply.github.com>
2022-09-12 17:21:03 -0300

    Add files via upload

---
+++ 05_portas_de_entrada_e_saida_i_o_s/s05_e11_botao_led_frequencia/s05_e11_botao_led_frequencia/main.asm
@@ -0,0 +1,52 @@
+;
+; s05_e11_botao_led_frequencia.asm
+;
+; Created: 23/08/2022 17:51:36
+; Author : Matheus
+;
+; 5.11 – Elaborar um programa para aumentar a frequência em que um LED liga e 
+; desliga, enquanto um botão estiver sendo pressionado, até o momento em que o 
+; LED ficará continuamente ligado. Quando o botão é solto o LED deve ser desligado.
+; Qual a aplicação prática dessa técnica, imaginando que o botão pode ser o sinal 
+; proveniente de algum sensor e o LED algum dispositivo sinalizador?
+
+;definições
+.EQU LED    = PD2         ;LED é substituído por PD2 no código
+.EQU BOTAO  = PD7         ;BOTAO é substituído por PD7 no código
+
+.ORG 0x00                 ;endereço de início de escrita do código
+
+INICIO:
+    LDI   R16, 0b00000100 ;carrega o registrador R16 com o valor 0b00000100
+    OUT   DDRD, R16       ;confira o pino RD2 do PORTD como saída e os restantes como entrada
+    LDI   R16, 0b11111011 ;carrega o registrador R16 com o valor 0b11111011
+    OUT   PORTD, R16      ;habilita os resistores pull-up para as entradas e desliga o led
+    NOP                   ;sincronização do PORT para permitir uma leitura imediatamente após uma escrita
+PRINCIPAL:
+    LDI   R16, 20         ;carrega o registrador R16 com o valor 42
+ testa_botao:
+    SBIC  PIND, BOTAO     ;verifica se o botão foi pressionado
+    RJMP  testa_botao     ;senão foi pressionado, vai para testa_botao
+    SBI   PORTD, LED      ;liga led
+    RCALL ATRASO          ;chama sub-rotina de atraso
+    CBI   PORTD, LED      ;desliga led
+    RCALL ATRASO          ;chama sub-rotina de atraso
+    DEC   R16             ;decrementa o registrador R16
+    CPI   R16, 0          ;verifica se o registrador R16 é igual a 0
+    BRNE  testa_botao     ;senão é igual a 0, vai para testa_botao
+    SBI   PORTD, LED      ;liga led 
+ mantem_ligado:
+    SBIS  PIND, BOTAO     ;verifica se o botão foi solto
+    RJMP  mantem_ligado   ;senão foi pressionado, vai para testa_botao
+    CBI   PORTD, LED      ;desliga led
+    RJMP  PRINCIPAL       ;retorna para o início da rotina, loop infinito
+ATRASO:                   ;gera inicialmente um atraso de aproximadamente 250ms 
+    MOV   R19, R16        ;move o valor do registrador R16 para o registrador R19
+ volta: 
+    DEC   R17             ;decrementa o registrador R17
+    BRNE  volta           ;enquanto R17 > 0, decrementa R17
+    DEC   R18             ;decrementa o registrador R18
+    BRNE  volta           ;enquanto R18 > 0, decrementa R18
+    DEC   R19             ;decrementa o registrador R19
+    BRNE  volta           ;enquanto R19 > 0, vai para volta
+    RET                   ;retorna para a rotina principal
---
+++ 05_portas_de_entrada_e_saida_i_o_s/s05_e12_selecao_animacao/s05_e12_selecao_animacao/main.c
@@ -0,0 +1,257 @@
+/*
+ * s05_e12_selecao_animacao.c
+ *
+ * Created: 24/08/2022 17:56:06
+ * Author : Matheus
+ * 5.12 – No exercício 5.8, foram propostas 7 animações com 8 LEDs. Crie outra
+ * animação, totalizando 8. Depois empregue dois botões: um será o AJUSTE, que
+ * quando pressionado permitirá que o outro botão (SELEÇÃO) selecione a função
+ * desejada, ver a fig. 5.11. Cada vez que o botão SELEÇÃO for pressionado, um
+ * dos oito LEDs deverá acender para indicar a função escolhida; exemplo:
+ * 00000100 => LED 3 ligado, função 3 selecionada. Quando o botão de AJUSTE for
+ * solto, o sistema começa a funcionar conforme a função escolhida.
+ * Desenvolva a programação por partes, unindo-as e testando com cuidado. Não
+ * esqueça: um bom programa é bem comentado e organizado!
+ */ 
+
+#define F_CPU 16000000UL  //define frequência do oscilador de 16MHz
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+//definições de macros
+#define set_bit(Y, bit_x) (Y |= (1 << bit_x)  //seta o bit x do PORTY
+#define clr_bit(Y, bit_x) (Y &= ~(1 << bit_x) //limpa o bit x do PORTY
+#define cpl_bit(Y, bit_x) (Y ^= (1 << bit_x)  //complementa o bit x do PORTY
+#define tst_bit(Y, bit_x) (Y & (1 << bit_x))  //testa o bit x do PORTY
+
+#define AJUSTE  PB0 //AJUSTE é substituído por PB0 na programação
+#define SELECAO PB1 //SELECAO é substituído por PB1 na programação
+
+//declarações de protótipos de funções
+void atraso_10ms(void);
+void atraso_250ms(void);
+void atraso_500ms(void);
+void animacao_1(void);
+void animacao_2(void);
+void animacao_3(void);
+void animacao_4(void);
+void animacao_5(void);
+void animacao_6(void);
+void animacao_7(void);
+void animacao_8(void);
+
+int main()
+{
+	DDRD = 0xFF;        //todo o PORTD é configurado como saída
+	DDRB = 0b11111100;  //os pinos PB0 e PB1 do PORTB são configurados como entrada e os demais como saída
+	PORTB = 0b00000011; //habilita os resistores pull-up das entradas
+	UCSR0B = 0x00;      //desabilita RXD e TXD
+
+	unsigned char animacao = 0; //variável local inicializada
+
+	while(1)
+	{
+		while(!tst_bit(PINB, AJUSTE))   //verifica se o botão AJUSTE foi pressionado
+		{
+			PORTD = animacao;           //acende led correspondente a animação selecionada
+			if(!tst_bit(PINB, SELECAO)) //verifica se o botão SELECAO foi pressionado
+			{
+				while(!tst_bit(PINB, SELECAO)); //verifica se o botão SELECAO foi solto
+				atraso_10ms();                  //atraso de 10ms para eliminar ruído do botão
+				animacao <<= 1;                 //seleciona a próxima animação
+				if(animacao == 0) animacao = 1;
+			} //teste do botão SELECAO
+		} //teste do botão AJUSTE
+		PORTD = 0x00;        //desliga todos os leds
+		atraso_500ms();      //atraso de 500ms
+		switch (animacao)    //verifica qual animação foi selecionada
+		{
+			case 0x01:
+			animacao_1();    //animação 1
+			break;
+			case 0x02:
+			animacao_2();    //animação 2
+			break;
+			case 0x04:
+			animacao_3();    //animação 3
+			break;
+			case 0x08:
+			animacao_4();    //animação 4
+			break;
+			case 0x10:
+			animacao_5();    //animação 5
+			break;
+			case 0x20:
+			animacao_6();    //animação 6
+			break;
+			case 0x40:
+			animacao_7();    //animação 7
+			break;
+			case 0x80:
+			animacao_8();    //animação 8
+			break;
+			default:
+			PORTD = 0x00;    //caso nenhuma animação esteja selecionada, desliga todos os leds
+			break;
+		} //teste animação
+		PORTD = 0x00;       //desliga todos os leds
+		animacao = 0;       //atribui novo valor à variável animacao
+	} //laço infinito
+}
+
+//implementação das funções
+
+//funções de tempo
+
+void atraso_10ms(void)
+{
+	_delay_ms(10);    //atraso de 10ms
+}
+
+void atraso_250ms(void)
+{
+	_delay_ms(250);    //atraso de 250ms
+}
+
+void atraso_500ms(void)
+{
+	_delay_ms(500);    //atraso de 500ms
+}
+
+//funções de animações
+
+//sequencial de leds deslocando da direita para a esquerda
+void animacao_1(void)
+{
+	unsigned char cont = 0x01;  //variável local inicializada
+	PORTD = cont;               //liga leds
+	atraso_500ms();             //atraso de 500ms
+	while(1)
+	{
+		cont <<= 1;             //deslocamento lógico para a esquerda
+		PORTD = cont;           //liga leds
+		atraso_500ms();         //atraso de 500ms
+		if(cont == 0x00) break; //sai ao completar o deslocamento
+	}
+}
+
+
+//sequencial de leds deslocando da esquerda para a direita
+void animacao_2(void)
+{
+	unsigned char cont = 0x80;  //variável local inicializada
+	PORTD = cont;               //liga ledas
+	atraso_500ms();             //atraso de 500ms
+	while(1)
+	{
+		PORTD = cont;           //liga ledas
+		cont >>= 1;             //deslocamento lógico para a direita
+		atraso_500ms();         //atraso de 500ms
+		if(cont == 0x00) break; //saí ao completar o deslocamento
+	}
+}
+
+
+//ligar os leds sequencialmente da esquerda para a direita (vai-e-volta)
+void animacao_3(void)
+{
+	unsigned char cont = 0x80;  //variável local inicializada
+	while(1)
+	{
+		PORTD = cont;           //liga leds
+		atraso_500ms();         //atraso de 500ms
+		cont >>= 1;             //deslocamento lógico para a direita
+		if(cont == 0x00) break; //sai ao completar o deslocamento
+	} //vai
+	cont = 0x02;
+	while(1)
+	{
+		PORTD = cont;           //liga leds
+		atraso_500ms();         //atraso de 500ms
+		cont <<= 1;             //deslocamento lógico para a esquerda
+		if(cont == 0x80) break; //sai ao completar o deslocamento
+	} //volta
+	PORTD = cont;               //liga leds
+	atraso_500ms();             //atraso de 500ms
+}
+
+
+//ligar todos os leds e apagar sequencialmente da direita para a esquerda (vai-e-volta)
+void animacao_4(void)
+{
+	unsigned char cont = 0xFF;  //variável local inicializada
+	while(1)
+	{
+		PORTD = cont;           //liga leds
+		atraso_500ms();         //L de 500ms
+		cont <<= 1;             //deslocamento lógico para a esquerda
+		if(cont == 0x00) break; //sai ao apagar todos os leds
+	} //vai
+	PORTD = cont;
+	atraso_500ms();             //atraso de 500ms
+	cont = 0xFF;
+	while(1)
+	{
+		PORTD = cont;           //liga leds
+		atraso_500ms();         //atraso de 500ms
+		cont >>= 1;             //deslocamento lógico para a direita
+		if(cont == 0x00) break; //sai ao apagar todos os leds
+	} //volta
+	PORTD = cont;               //liga leds
+	atraso_500ms();             //atraso de 500ms
+}
+
+//ligar leds sequencialmente da direita para a esquerda um de cada vez mantendo o anterior aceso
+void animacao_5(void)
+{
+	unsigned char cont = 0x00;  //variável local inicializada
+	PORTD = cont;               //liga leds
+	atraso_500ms();             //atraso de 500ms
+	while(1)
+	{
+		cont = (cont << 1) + 1; //deslocamento para a esquerda e incremento
+		PORTD = cont;           //liga leds
+		atraso_500ms();         //atraso de 500ms
+		if(cont == 0xFF) break; //sai se todos os led estiverem acesos
+	}
+}
+
+//ligar leds sequencialmente da esquerda para a direita um de cada vez mantendo o anterior aceso
+void animacao_6(void)
+{
+	unsigned char cont = 0x00;  //variável local inicializada
+	PORTD = cont;               //liga leds
+	atraso_500ms();             //atraso de 500ms
+	while(1)
+	{
+		cont = (cont >> 1) + 0x80; //deslocamento para a direita e incremento de 0x80
+		PORTD = cont;               //liga leds
+		atraso_500ms();             //atraso de 500ms
+		if(cont == 0xFF) break; //sai se todos os led estiverem acesos
+	}
+}
+
+//contagem binária crescente de 0 - 255
+void animacao_7(void)
+{
+	unsigned char cont = 0; //variável local inicializada
+	while(cont < 256)
+	{
+		PORTD = cont;      //liga leds
+		atraso_250ms();    //atraso de 250ms
+		cont++;            //incrementa contador
+	}
+}
+
+//contagem binária decrescente de 255 - 0
+void animacao_8(void)
+{
+	unsigned char cont = 255; //variável local inicializada
+	while(cont >= 0)
+	{
+		PORTD = cont;         //liga leds
+		atraso_250ms();       //atraso de 250ms
+		cont--;               //decrementa variável
+	}
+}
