7df0498e6b245de92555bfd2ca76e10b0bd3848a
MatheusDeORibeiro <89406578+MatheusDeORibeiro@users.noreply.github.com>
2022-09-12 16:29:19 -0300

    Add files via upload

---
+++ 05_portas_de_entrada_e_saida_i_o_s/Pisca_LED_asm/Pisca_LED_asm/main.asm
@@ -0,0 +1,34 @@
+;
+; Pisca_LED_asm.asm
+;
+; Created: 02/08/2022 15:49:14
+; Author : Matheus
+;
+
+
+//------------------------------------------------------------------------
+.equ LED = PB5 //LED é o substituto de PB5 na programação
+.ORG 0x000 //endereço de início de escrita do código
+
+INICIO:
+    LDI R16,0xFF //carrega R16 com o valor 0xFF
+    OUT DDRB,R16 //configura todos os pinos do PORTB como saída
+
+PRINCIPAL:
+    SBI   PORTB, LED //coloca o pino PB5 em 5V
+    RCALL ATRASO     //chama a sub-rotina de atraso
+    CBI   PORTB, LED //coloca o pino PB5 em 0V
+    RCALL ATRASO     //chama a sub-rotina de atraso
+    RJMP  PRINCIPAL  //volta para PRINCIPAL
+
+ATRASO:   //atraso de aprox. 200 ms
+    LDI   R19,16
+ volta:
+    DEC   R17   //decrementa R17, começa com 0x00
+    BRNE  volta //enquanto R17 > 0 fica decrementando R17
+    DEC   R18   //decrementa R18, começa com 0x00
+    BRNE  volta //enquanto R18 > 0 volta a decrementar R18
+    DEC   R19   //decrementa R19
+    BRNE  volta //enquanto R19 > 0 vai para volta
+    RET
+//------------------------------------------------------------------------
