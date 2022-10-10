d3bcd35d91ec9c6a2118beab158ace9693f17e3f
Matheus de Oliveira Ribeiro <89406578+MatheusDeORibeiro@users.noreply.github.com>
2022-10-10 18:13:41 -0300

    Add files via upload

---
+++ 07_gravando_a_EEPROM/s07_e01_escrevendo_xAA_em_toda_a_EEPROM/s07_e01_escrevendo_xAA_em_toda_a_EEPROM/main.c
@@ -0,0 +1,79 @@
+/*
+ * s07_e01_escrevendo_xAA_em_toda_a_EEPROM.c
+ *
+ * Created: 28/09/2022 16:50:28
+ * Author : Matheus
+ *
+ * 7.1 – Faça um programa para gravar 0xAA em toda a memória EEPROM do ATmega328.
+ * Certifique-se que os dados foram corretamente gravados, lendo-os após a
+ * gravação. Pode ser empregado um display para informar sobre o sucesso da
+ * operação ou um LED. Depois disso, com base no manual do ATmega328, empregue a
+ * interrupção da EEPROM para repetir o processo.
+ */
+
+#include "definicoes_principais.h" //inclui a biblioteca de definições principais
+
+
+//definição de hardware
+#define LED PB5 //substitui LED por PB5 na programação
+
+//rotina de tratamento de interrupção
+ISR(EE_READY_vect);
+
+int main(void) //função principal
+{
+	DDRB = 0xFF; //define todo o PORTB como saída
+	PORTB = 0x00; //coloca todo o PORTB em nível baixo
+
+	EECR = (1 << EERIE); //habilita a interrupção da EEPROM
+	sei(); //seta o bit I do registrador SREG para habilitar todas as interrupções
+
+	while(1); //loop infinito
+}
+
+//implementando rotina de tratamento de interrupção
+
+ISR(EE_READY_vect)
+{
+    static unsigned int end_eeprom = 0x00; //declaração de variável local estática
+	
+	if(end_eeprom <= 0x03FF) //escrita de 0x0000 até 0x3FF
+	{
+		cli(); //limba o bit I do registrador SREG desabilitando todas as interrupções
+		/* Aguarda completar a escrita anterior */
+		while(EECR & (1 << EEPE));
+		/* Carrega os registradores de endereço e dado */
+		EEAR = end_eeprom++;
+		EEDR = 0xAA;
+		/* Escreve um lógico em EEMPE */
+		EECR |= (1 << EEMPE);
+		/* Inicia a escrita na eeprom setando EEPE */
+		EECR |= (1 << EEPE);
+		sei(); //seta o bit I do registrador SREG para habilitar todas as interrupções
+	}
+	else if(end_eeprom <= 0x07FE) //leitura de 0x04000 até 0x07FE
+	{
+		unsigned char leitura_eeprom; //variável auxiliar
+		/* Aguarda completar a escrita anterior */
+		while(EECR & (1 << EEPE));
+		/* Carrega o registrador de endereço */
+		EEAR = end_eeprom - 0x03FF; //end_eeprom++;
+		/* Inicia a leitrua da eeprom setando EERE */
+		EECR |= (1 << EERE);
+		/* Armazena o dado dReturn data from Data Register */
+		leitura_eeprom = EEDR;
+		if(leitura_eeprom == 0xAA) set_bit(PORTB, LED); //se o dado foi gravado corretamente, liga o led
+		else
+		{
+			clr_bit(PORTB, LED); //se o dado foi gravado incorretamente, desliga o led
+			EECR = ~(1 << EERIE); //desativa a interrupção da eeprom
+			cli(); //limpa o bit I do registrador SREG desabilitando todas as interrupções
+		}
+		end_eeprom++; //incremeta o endereço da eeprom
+	}
+	else
+	{
+		EECR = ~(1 << EERIE); //desabilita a interrupção da eeprom
+		cli(); //limpa o bit I do registrador SREG desabilitando todas as interrupções
+	}
+}
---
+++ 07_gravando_a_EEPROM/s07_e02_inicializacao_ci/s07_e02_inicializacao_ci/main.c
@@ -0,0 +1,70 @@
+/*
+ * s07_e02_inicializacao_ci.c
+ *
+ * Created: 29/09/2022 18:49:05
+ * Author : Matheus
+ * 
+ * 7.2 – Supondo que exista um circuito integrado ligado ao ATmega328 e que a
+ * primeira vez que o sistema é energizado esse circuito necessite uma
+ * inicialização específica. O programa do microcontrolador deve interagir com o
+ * usuário e perguntar se a inicialização do circuito integrado deve ser feita;
+ * caso afirmativo, essa ação será feita somente uma vez e, na próxima vez que o
+ * microcontrolador for inicializado, o programa não fará mais nenhuma pergunta.
+ *
+ * Faça um programa que utilize um byte da EEPROM para armazenar a informação de
+ * que a inicialização do circuito integrado foi feita. Empregue dois LEDs e um
+ * botão. A primeira vez, um dos LEDs acende, indicando que a inicialização
+ * necessita ser feita; quando o botão for pressionado, o segundo LED deve ser
+ * ligado. Ao se reenergizar o circuito, somente o segundo LED deve ficar ligado,
+ * indicando que a inicialização já foi feita e o processo de inicialização não é
+ * mais necessário.
+ */ 
+
+#include "definicoes_principais.h" //inclui a biblioteca de definições principais
+
+
+//definições de hardware
+#define LED1  PD0 //substitui LED1 por PD0 na programação
+#define LED2  PD1 //substitui LED2 por PD1 na programação
+#define BOTAO PD2 //substitui BOTAO por PD2 na programação
+
+int main(void) //função principal
+{
+	unsigned char dado = 0x50;
+	unsigned int endereco_eeprom = 0x000F; //endereço que a informação da inicialização será armazenada
+	unsigned char leitura_eeprom;
+
+	DDRD = 0xFB; //configura o pino PD2 como entrada e os pinos restantes como saída
+	PORTD = 0x04; //habilita o resistor pull-up da entrada e coloca os pinos restantes em nível baixo
+
+	UCSR0B = 0x00; //configura os pinos RX e TX (PD0 e PD1) como I/Os digitais para o Arduino Uno
+
+	__EEGET(leitura_eeprom, endereco_eeprom);
+	
+	if(leitura_eeprom == dado) //verifica se a inicialização é necessária
+	{
+		set_bit(PORTD, LED2); //liga LED que indica que a inicialização não precisa ser mais feita
+	}
+	else
+	{
+		while(leitura_eeprom != dado)
+		{
+			set_bit(PORTD, LED1);      //acende o LED que indica que a inicialização deve ser feita
+			if(!tst_bit(PIND, BOTAO)) //aguarda o botão ser pressionado
+			{
+				while(!tst_bit(PIND, BOTAO));  //aguarda o botão ser solto
+				__EEPUT(endereco_eeprom, dado); //armazena a informação na EEPROM
+				//inicialização do circuito integrado
+			}
+			__EEGET(leitura_eeprom, endereco_eeprom); //leitua da informação armazenada
+			if(leitura_eeprom == dado)                //verifica se a informação armazenada está correta
+			{
+				clr_bit(PORTD, LED1); //apaga LED1
+				set_bit(PORTD, LED2); //acende LED2
+			}
+		}
+	}
+	while(1) //loop inifinito
+	{
+	}
+}
