52b4078e1616215850a18072d5a3fa810b490932
Matheus de Oliveira Ribeiro <89406578+MatheusDeORibeiro@users.noreply.github.com>
2022-12-20 17:06:58 -0300

    Add files via upload

---
+++ 09_temporizadores_contadores/s09_e01_pisca_led/s09_e01_pisca_led/main.c
@@ -0,0 +1,39 @@
+/*
+ * s09_e01_pisca_led.c
+ *
+ * Created: 11/10/2022 18:31:59
+ * Author : Matheus
+ * 
+ * 9.1 – Considerando o TC0 trabalhando a 8 MHz com um prescaler de 256, quanto
+ * tempo leva para o TC0 gerar um estouro de contagem?
+ *
+ * t_estouro = ((TOP + 1) * prescaler) / f_osc =
+ * - TC0 -> 8 bits -> TOP = 255
+ * t_estouro = ((255 + 1) * 256) / 8M
+ * t_estouro = 8,192ms.
+ */ 
+
+#include "definicoes_principais.h" //inclui as definições principais
+
+
+//definição de hardware
+#define LED PB5 //substitui LED por PB5 na programação
+
+ISR(TIMER0_OVF_vect) //rotina de interrupção
+{
+	cpl_bit(PORTB, LED); //muda o estado do LED a cada 8,192ms
+}
+
+int main(void) //função principal
+{
+	DDRB = 0xFF;  //configura todo o PORTB como saída
+	PORTB = 0x00; //coloca todo o PORTB em nível baixo
+
+	TCCR0B = 1 << CS02;  //configura o prescaler igual a 256
+	/* modo normal, prescaler igual a 256 e f_osc de 8MHz: t_estouro = 8,192ms*/
+	
+	TIMSK0 = 1 << TOIE0; //habilita a interrupção do estouro do TC0
+	sei();               //habilita todas as fontes de interrupção
+
+	while(1); //loop infinito
+}
---
+++ 09_temporizadores_contadores/s09_e02_1_pwm/s09_e02_1_pwm/main.c
@@ -0,0 +1,33 @@
+/*
+ * s09_e02_1_pwm.c
+ *
+ * Created: 12/10/2022 14:30:30
+ * Author : Matheus
+ *
+ * 9.2 – Considerando-se o PWM rápido configurado para que o valor máximo de
+ * contagem do TC0 seja dado pelo valor de OCR0A e que o pino OC0B esteja
+ * configurado para gerar o sinal PWM, determine:
+ *
+ * - Qual o período do sinal PWM se o ATmega328 estiver rodando a 12 MHz e o
+ * valor de OCR0A for 99?
+ * T_oc0a_pwm = f_oc0a_pwm ^ -1 = (f_osc / (N * (1 + TOP)) ^ -1
+ * T_oc0a_pwm = (12M / (1 * (1 + 99)) ^ -1 -> T_oc0a_pwm = 8,33us
+ */ 
+
+#include "definicoes_principais.h" //inclui a biblioteca de definições principais
+
+
+int main(void) //função principal
+{
+    DDRD = 0xFF;  //configura todo o PORTD como saída
+    PORTD = 0x00; //coloca todo o PORTD em nível baixo
+
+    TCCR0A = 0b01100011; //TOP = OCR0A, PWM é gerado por OCR0B e o modo PWM rápido
+    TCCR0B = 0b00001001; //compara TCNT0 com OCR0A e prescaler = 1
+    OCR0A = 99; //TOP
+    /* modo PWM rápido, f_osc de 12MHz, prescaler igual a 1, TOP igual a 99: 
+       f_oc0a_pwm = 120kHz e T_oc0a_pwm = 8,33us */
+
+    while(1); //loop infinito
+}
+
